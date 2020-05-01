f9db31ef05bcc11824818495ffeb3dccf64e7e59
Imke Van Rompa <ivanrompa@gmail.com>
2020-05-01 09:35:38 +0200

    refactor initial server setup

--- bissasist-node/src/index.js
+++ bissasist-node/src/index.js
@@ -1,38 +1,34 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-type Query {
-    info: String!
-    servicefeed: [Service!]!
-}
-
-type Service {
-    id: ID!
-    name: String!
-    cost: String!
-}
-`
-let Service = [{
-    id:'service-0',
+let services = [{
+    id: 'service-0',
     name: 'Follow up',
     cost: "15 euro"
 }]
 
-const resolvers ={
+let idCount = services.length
+
+const resolvers = {
     Query: {
         info: () => `Bussines Scheduler API`,
-        servicefeed: () => service,
+        servicefeed: () => services,
     },
+    Mutation: {
+        post: (parent, args) => {
+            const service = {
+                id: `service-${idCount++}`,
+                name: args.name,
+                cost: args.cost
+            }
+            services.push(service)
+            return service
 
-    Service:{
-        id: (parent) => parent.id,
-        name: (parent)=> parent.name,
-        cost: (parent ) => parent.cost,
+        }
     }
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
