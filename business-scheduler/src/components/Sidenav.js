6228d03690cf6257d49e8ed85f2714ab69e280ca
Imke Van Rompa <ivanrompa@gmail.com>
2020-05-02 15:22:12 +0200

    Login & signup
    logic tested FE & BE

--- business-scheduler/src/components/Sidenav.js
+++ business-scheduler/src/components/Sidenav.js
@@ -8,7 +8,7 @@ class Sidenav extends Component {
   return (
     <div className="flex pa1 justify-between nowrap orange">
       <div className="flex flex-fixed black">
-        <div className="fw7 mr1">Hacker News</div>
+        <div className="fw7 mr1">Bees - Business Scheduler</div>
         <Link to="/" className="ml1 no-underline black">
           All services
         </Link>
